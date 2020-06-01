4cfda432d4405905b4e7c4c053be342f4ab9aae3
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 07:35:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   end
   
   min_value = 0  
-  name_hash.collect do |name, value|
+  name_hash.each do |name, value|
     
     if min_value > value
       min_value = value 
