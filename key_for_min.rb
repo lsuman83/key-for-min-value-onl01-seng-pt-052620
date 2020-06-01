db47f4960dd61d1021a4dc927f5c3669edcf83ce
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-01 08:17:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+    return nil
+  end
+  
+  min_value = 0  
+  name_hash.collect do |name, value|
+    
+    if min_value == 0 || min_value > value
+      min_value = value 
+    end
+    
+  end  
+  
+  name_hash.each do |name, value|
+    
+    if value == min_value
+      return name
+    end
+  end
 end
